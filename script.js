1a8807952d3f32738cfcf4dcbec921d82545867e
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2025-12-01 00:35:36 +0300

    feat: implement bookmark addition functionality and enhance delete button styles

---
+++ script.js
@@ -0,0 +1,21 @@
+const addBookmarkBtn = document.getElementById('add-bookmark-btn');
+const bookmarkList = document.getElementById('bookmark-list');
+const bookmarkURLInput = document.getElementById('bookmark-url');
+const bookmarkNameInput = document.getElementById('bookmark-name');
+
+addBookmarkBtn.addEventListener('click',() =>{
+    const url = bookmarkURLInput.value;
+    const name = bookmarkNameInput.value;
+
+    if(url && name){
+        const listItem = document.createElement('li');
+        const link = document.createElement('a');
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Delete';
+        link.href = url;
+        link.textContent = name;
+        link.target = '_blank';
+        listItem.appendChild(link);
+        bookmarkList.appendChild(listItem); 
+    }
+})
